6f21647d9a78a164e3e6490e91937636bfdc4c5d
Ashwin-deals <ashwin11evr@gmail.com>
2025-11-13 23:18:27 +0530

    Deployment

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,3 @@
-# Create your views here.
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -16,11 +14,9 @@ from profiles_api import models
 from profiles_api import permissions
 
 class HelloApiView(APIView):
-    # Test API View
     serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
-        # Returns a list of APIView features.
         an_apiview = [
             'Uses HTTP methods as function (get, post, patch, delete, put)',
             'Is similar to a traditional Django View',
@@ -31,7 +27,6 @@ class HelloApiView(APIView):
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
 
     def post(self, request):
-        # Create a hello message with our name.
         serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
@@ -45,23 +40,18 @@ class HelloApiView(APIView):
             )
 
     def put(self, request, pk=None):
-        # Handle updating an object.
         return Response({'method': 'PUT'})
 
     def patch(self, request, pk=None):
-        # Handle a partial update of an object.
         return Response({'method': 'PATCH'})
 
     def delete(self, request, pk=None):
-        # Delete an object.
         return Response({'method': 'DELETE'})
 
 class HelloViewSet(viewsets.ViewSet):
-    # Test API ViewSet
     serializer_class = serializers.HelloSerializer
 
     def list(self, request):
-        # Return a hello message
         a_viewset = [
             'Uses actions (list, create, retrieve, update, partial_update)',
             'Automatically maps to URLs using Routers',
@@ -71,7 +61,6 @@ class HelloViewSet(viewsets.ViewSet):
         return Response({'message': 'Hello!', 'a_viewset': a_viewset})
 
     def create(self, request):
-        # Create a new hello message
         serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
@@ -85,46 +74,35 @@ class HelloViewSet(viewsets.ViewSet):
             )
 
     def retrieve(self, request, pk=None):
-        # Handle getting an objest by its ID
         return Response({'http_method': 'GET'})
 
     def update(self, request, pk=None):
-        # Handle updating an object
         return Response({'http_method': 'PUT'})
 
     def partial_update(self, request, pk=None):
-        # Handle updating part of an object
         return Response({'http_method': 'PATCH'})
 
     def destroy(self, request, pk=None):
-        # Handle removing an object
         return Response({'http_method': 'DELETE'})
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
-    # Handlde creating and updating profiles
     serializer_class = serializers.UserProfileSerializer
-
-    # The server gets access to everything.
     queryset = models.UserProfile.objects.all()
-
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
 
 class UserLoginApiView(ObtainAuthToken):
-    # Handle creating user authentication tokens.
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
-class UserProfileViewSet(viewsets.ModelViewSet):
-    # Handles creating, reading and updating profile feed items.
+class ProfileFeedItemViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
     permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
 
     def perform_create(self, serializer):
-        # Sets the user profile to the logged in user
         serializer.save(user_profile=self.request.user)
