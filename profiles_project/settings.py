11522eef50bb09dddbb4bbb5282c47daca25bdfe
Ashwin-deals <ashwin11evr@gmail.com>
2025-11-13 22:30:01 +0530

    Configured for deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rk10y9w0us%7atr2=+v(ax_ffdwy9=72kgke+ig2e%vo)3877j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -122,4 +122,4 @@ STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
 
-
+STATIC_ROOT = 'static/'
